b771d98320a7b551d33f40905279537da0ee3ffc
pang-gohan <ipad1117@gmail.com>
2023-10-04 16:28:58 +0900

    新規作成機能の一通り作成

--- app.rb
+++ app.rb
@@ -18,20 +18,7 @@ post '/new' do
   # フォームから送信されたデータを取得
   subject = params['subject']
   content = params['content']
-
-  # 新しいデータを作成
-  new_memo = {
-    "id": memos.length + 1,
-    "subject": subject,
-    "content": content
-  }
-
-  # 新しいデータを既存のデータに追加
-  memos << new_memo.transform_keys(&:to_s)
-  # 更新後のデータをJSON形式にシリアライズしてファイルに保存
-  File.open('data/memos.json', 'w') do |file|
-    file.write(JSON.pretty_generate(memos))
-  end
+  edit_memo(subject, content, nil, memos)
   redirect '/'
 end
 
@@ -49,6 +36,21 @@ end
 get '/:memo_id/delete' do
   @memo_id = params[:memo_id].to_i
   erb :delete
+end
+
+def edit_memo(subject, content, memo_id, memos)
+  # 新しいデータを作成
+  new_memo = {
+    "id": memo_id.nil? ? memos.length + 1 : memo_id,
+    "subject": subject,
+    "content": content
+  }
+  # 新しいデータを既存のデータに追加
+  memos << new_memo.transform_keys(&:to_s)
+  # 更新後のデータをJSON形式にシリアライズしてファイルに保存
+  File.open('data/memos.json', 'w') do |file|
+    file.write(JSON.pretty_generate(memos))
+  end
 end
 
 # bundle exec ruby app.rb
