f5d61bf5edb827fa9ba3e7017fa01e897b0a1e33
pang-gohan <ipad1117@gmail.com>
2023-10-23 10:47:34 +0900

    プリペアドステートメント対応。README対応。

--- db.rb
+++ db.rb
@@ -8,12 +8,18 @@ DB_PARAMS = {
   host: 'localhost',      # ホスト名
   port: 5432,             # ポート番号
   dbname: 'memo_db',      # データベース名
-  user: 'yoshinori', # データベースユーザー名
-  password: 'hoehoe' # パスワード
+  user: 'your_name', # データベースユーザー名
+  password: 'your_password' # パスワード
 }.freeze
 
 def close_connection(connection)
   connection&.close
+end
+
+def prepare_statements(connection)
+  connection.prepare('insert_memo', 'INSERT INTO memos (subject, content) VALUES ($1, $2)')
+  connection.prepare('delete_memo', 'DELETE FROM memos WHERE id = $1')
+  connection.prepare('update_memo', 'UPDATE memos SET subject = $1, content = $2 WHERE id = $3')
 end
 
 # データベースから読み込み。JSON形式へ
@@ -39,7 +45,8 @@ end
 def add_memo(new_memo)
   connection = PG.connect(DB_PARAMS)
   begin
-    connection.exec_params('INSERT INTO memos (subject, content) VALUES ($1, $2)', [new_memo.subject, new_memo.content])
+    prepare_statements(connection)
+    connection.exec_prepared('insert_memo', [new_memo.subject, new_memo.content])
   ensure
     close_connection(connection)
   end
@@ -48,7 +55,8 @@ end
 def delete_memo(memo_id)
   connection = PG.connect(DB_PARAMS)
   begin
-    connection.exec_params('DELETE FROM memos WHERE id = $1', [memo_id])
+    prepare_statements(connection)
+    connection.exec_prepared('delete_memo', [memo_id])
   ensure
     close_connection(connection)
   end
@@ -57,7 +65,8 @@ end
 def save_memos(target_memo)
   connection = PG.connect(DB_PARAMS)
   begin
-    connection.exec_params('UPDATE memos SET subject = $1, content = $2 WHERE id = $3', [target_memo.subject, target_memo.content, target_memo.id])
+    prepare_statements(connection)
+    connection.exec_prepared('update_memo', [target_memo.subject, target_memo.content, target_memo.id])
   ensure
     close_connection(connection)
   end
